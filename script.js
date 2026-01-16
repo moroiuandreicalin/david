7f4355151297334c55837cce2d58343e5ad14d64
moroiuandreicalin <moroiuandreicalin@gmail.com>
2026-01-16 20:45:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openToate_Articolele() {
+	window.location.href = 'Blogul_Meu.html';
+}
+
+function openJocuri_Video() {
+	window.location.href = 'Jocuri_Video.html';
+}
+
+function openContact() {
+	window.location.href = 'Contact.html';
+}
